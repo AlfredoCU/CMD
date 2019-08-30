cb8ba1260906dfbaa50b9b10b457d329146ab9b6
AlfredoCU <alfredo_11_cu@hotmail.com>
2019-08-30 18:38:22 -0500

    Se cre un sistema para manipular comandos cmd.exe

---
+++ Practica_1_CMD/Info.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_1_CMD
+{
+    public partial class Info : Form
+    {
+        public Info()
+        {
+            InitializeComponent();
+        }
+    }
+}
